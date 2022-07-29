737933983bd17feb843a76ec5c7334250ac12798
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2022-07-29 20:57:16 +0800

    feat: add webchat forwarding backend
    * refactor: remove unused function in pch.h

--- BDSLM/apiServer.cpp
+++ BDSLM/apiServer.cpp
@@ -1,22 +1,34 @@
 #include "pch.h"
 #include <httplib/httplib.h>
 #include "conf.h"
+#include "webChat.h"
 
 int startApiServer() {
-	httplib::Server svr;
+    httplib::Server svr;
 
-	svr.Get("/api/info", [](const httplib::Request&, httplib::Response& res) {
-		res.set_content("BDSLM v0.3.2", "text/plain");
-	});
+    svr.Get("/api/info", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("BDSLM v0.3.2", "text/plain");
+    });
 
-	svr.Get("/api/getPlayerMarkers", [](const httplib::Request&, httplib::Response& res) {
-		res.set_content(getPlayerMarkers(), "application/json");
-	});
+    svr.Get("/api/getPlayerMarkers", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content(getPlayerMarkers(), "application/json");
+    });
 
-	svr.Post("/api/chat/fetch", [](const httplib::Request& req, httplib::Response& res) {
-		
-	});
+    if (config.enableWebChat) {
+        svr.Get("/api/chat/fetch", [](const httplib::Request& req, httplib::Response& res) {
+            
+            if (req.params.contains("start")) {
+                auto range = req.params.equal_range("start");
+                int id = std::stoi(range.first->second);
+                nlohmann::json j(webChatService.getMessages(id));
+                res.set_content(j.dump(), "application/json");
+                return;
+            }
+            nlohmann::json j(webChatService.msg_list_);
+            res.set_content(j.dump(), "application/json");
+        });
+    }
 
-	svr.listen("0.0.0.0", config.apiServerPort);
-	return 0;
+    svr.listen("0.0.0.0", config.apiServerPort);
+    return 0;
 }
--- BDSLM/pluginInit.cpp
+++ BDSLM/pluginInit.cpp
@@ -1,10 +1,14 @@
 #include "pch.h"
 #include <thread>
 #include "conf.h"
+#include "webChat.h"
 
 int pluginInit() {
     std::ios::sync_with_stdio(false);
     config.load();
+    if (config.enableWebChat) {
+        webChatService.init();
+    }
     markersInit();
     std::thread apiServerThread(startApiServer);
     apiServerThread.detach();
